7c43fb89c02c04a3a9b27163c4fb60fc2112dafa
gracebir <grace.devolop@gmail.com>
2026-02-27 18:29:28 +0200

    setting vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      task management
-    </div>
-  )
+      <div>
+          <h1 className='text-center text-xl'>task management</h1>
+      </div>
+  );
 }
 
 export default App
